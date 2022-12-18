504b88392fb870c8c0b8cc1d9a2593b5b5f55f19
Antonio Fialho <80779070+AntonioFialhoSN@users.noreply.github.com>
2022-12-18 00:35:47 -0300

    16 (#9)
    * teste 2
    
    * teste 4
    
    * teste 6
    
    * teste 7
    
    mascara table 3
    
    * Delete Conn.php
    
    * teste10
    
    teste11
    
    * teste 10
    
    teste 10
    
    * 14
    
    14
    
    Co-authored-by: Antonio Marcos Patricio Castro <54753638+TonyMPCastro@users.noreply.github.com>

--- app/Views/public_cliente/marcacao.php
+++ app/Views/public_cliente/marcacao.php
@@ -1,26 +1,25 @@
 <?php
 
 if (!defined('4578S9')) {
-  header("Location: /");
-  die("Erro: Página não encontrada!");
+    header("Location: /");
+    die("Erro: Página não encontrada!");
 }
 if (isset($this->dados['menu'])) {
-  $menu = $this->dados['menu'];
+    $menu = $this->dados['menu'];
 }
 
 if (isset($this->dados['marcacao_servico'])) {
-  $marcacao_servico = $this->dados['marcacao_servico'];
-} else {
+    $marcacao_servico = $this->dados['marcacao_servico'];
+}else{
   $marcacao_servico = [];
 }
 $table = 3;
 ?>
-<?php
+<?php 
 //include "app/Views/menu_footer/menu.php"; 
 // Incluir para chamada de 
 require_once("app/Views/menu_footer/menu.php");
 require_once("app/Lib/Mask.php");
-
 use app\Lib\Mask;
 
 
@@ -31,9 +30,9 @@ use app\Lib\Mask;
 <!-- partial -->
 <div class="main-panel">
   <div class="content-wrapper">
-    <a href="<?php echo URL . 'home2/index' ?>" class="menu-link" title="Home"><i class="mdi mdi-home"></i></a>
+    <a href="<?php echo URL . 'home/onShow' ?>" class="menu-link" title="Home"><i class="mdi mdi-home"></i></a>
     >
-    <a href="<?php echo URL . 'home2/index' ?>" class="menu-link"><span>MARCAÇAO DE HORARIOS</span></a>
+    <a href="<?php echo URL . 'servico_adm/index' ?>" class="menu-link"><span>MARCAÇAO DE HORARIOS</span></a>
 
     <br>
     <br>
@@ -43,12 +42,13 @@ use app\Lib\Mask;
       </div>
       <br>
       <div class="text-center">
-        <a href="<?php echo URL . 'home2/onShow' ?>" class="btn btn-outline-success btn-lg">CADASTRAR</a>
+        <a href="<?php echo URL . 'servico_adm/onShow' ?>" class="btn btn-outline-success btn-lg">CADASTRAR</a>
       </div>
       <br>
       <table id="example<?= $table; ?>" class="display" style="width:100%">
         <thead>
           <tr>
+            <th>#</th>
             <th>Serviço</th>
             <th>forma de pagamento</th>
             <!-- <th>Usuario</th> -->
@@ -61,11 +61,20 @@ use app\Lib\Mask;
           <?php if (count($marcacao_servico) > 0) {
             foreach ($marcacao_servico as $marcacao_servico_1) { ?>
               <tr>
+                <td style='font-size:28px'>
+                  <a href="<?php echo URL . 'servico_adm/onEdit?id=' . $marcacao_servico_1->id; ?>" title="Editar" data-toggle="popover" data-trigger="hover" data-content="Some content">
+                    <i style="color:#0090e7;" class='mdi mdi-account-plus'></i>
+                  </a>
+                  &nbsp;&nbsp;&nbsp;
+                  <a href="#" title="Apagar" data-toggle="modal" data-target="#confirm">
+                    <i style="color:red;" class='mdi mdi-delete'></i>
+                  </a>
+                </td>
                 <td><?= $marcacao_servico_1->servicos; ?></td>
-                <td><?= $marcacao_servico_1->forma_pagamento_id ?></td>
-                <!-- <td><?= $marcacao_servico_1->user_id; ?></td> -->
-                <td><?= ($marcacao_servico_1->horario_agendado) ? date("d/m/Y H:i:s", strtotime($marcacao_servico_1->horario_agendado)) : "NÃO MARCADO" ?></td>
-                <td><?= $marcacao_servico_1->status; ?></td>
+                <td><?= $marcacao_servico_1->nome ?></td>
+                <!-- <td>'<= $marcacao_servico_1->user_id; ?>'</td> -->
+                <td><?= $marcacao_servico_1->horario_agendado ?></td>
+                <td><?= $marcacao_servico_1->status == "1"? "ATIVO":"INATIVO";?></td>
               </tr>
           <?php  }
           }
@@ -73,6 +82,7 @@ use app\Lib\Mask;
         </tbody>
         <tfoot>
           <tr>
+          <th>#</th>
             <th>Serviço</th>
             <th>forma de pagamento</th>
             <!-- <th>Usuario</th> -->
