def6f1fcc7267781579da5386f81e67f7a723877
TonyMPCastro <antonio.m.fg38@outlook.com>
2022-12-18 01:35:29 -0300

    Update cad_pagamentos.php

--- app/Views/cliente/cad_pagamentos.php
+++ app/Views/cliente/cad_pagamentos.php
@@ -6,8 +6,10 @@ if (!defined('4578S9')) {
 }
 
 
-if (isset($this->dados['servico'])) {
-  $servico = $this->dados['servico'];
+if (isset($this->dados['f_pagamento'])) {
+  $f_pagamento = $this->dados['f_pagamento'];
+} else {
+  $f_pagamento = null;
 }
 
 ?>
@@ -34,32 +36,75 @@ if (isset($this->dados['servico'])) {
         <div class="card-body">
           <div class="row">
 
-            <div class="col-sm-4 col-lg-4">
+            <div class="col-sm-8 col-lg-8">
               <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-12 text-left control-label col-form-label">Descrição: <span style="color: red;" data-uw-styling-context="true">*</span></label>
+                <label for="inputEmail3" class="col-sm-12 text-left control-label col-form-label">Nome: <span style="color: red;" data-uw-styling-context="true">*</span></label>
                 <div class="col-sm-12 input-group has-validation">
-                  <input type="text" class="form-control" id="descricao" name="descricao" value="<?php echo isset($servico->descricao) ? $servico->descricao : ""; ?>" placeholder="Descrição do Serviço" required>
+                  <input type="text" class="form-control" id="nome" name="nome" value="<?php echo isset($f_pagamento->nome) ? $f_pagamento->nome : ""; ?>" placeholder="Descrição do Serviço" required>
                 </div>
               </div>
             </div>
 
+
             <div class="col-sm-4 col-lg-4">
               <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-12 text-left control-label col-form-label">Valor: <span style="color: red;" data-uw-styling-context="true">*</span></label>
+                <label for="inputEmail3" class="col-sm-12 text-left control-label col-form-label">Taxa: <span style="color: red;" data-uw-styling-context="true">*</span></label>
                 <div class="col-sm-12 input-group has-validation">
-                  <input type="number" min="0" step="0.01" class="form-control" id="valor" name="valor" value="<?php echo isset($servico->valor) ? $servico->valor : ""; ?>" placeholder="Valor" required>
+                  <input type="number" min="0" step="0.01" class="form-control" id="taxa" name="taxa" value="<?php echo isset($f_pagamento->taxa) ? $f_pagamento->taxa : ""; ?>" placeholder="taxa" required>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-sm-6 col-lg-6">
+              <div class="form-group row ">
+                <label for="validationCustom01" class="col-sm-12 text-left control-label col-form-label">Tipo Taxa: <span style="color: red;" data-uw-styling-context="true">*</span></label>
+                <div class="col-sm-12  has-validation">
+
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" id="tipo_taxa" name="tipo_taxa" value="1" required <?php echo isset($f_pagamento->tipo_taxa) ? (($f_pagamento->tipo_taxas == 1) ? 'checked' : '') : ""; ?>>
+                    <label class="form-check-label" for="DESATIVADO">
+                      FIXA
+                    </label>
+                  </div>
+
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" id="tipo_taxa" name="tipo_taxa" value="2" required <?php echo isset($f_pagamento->tipo_taxa) ? (($f_pagamento->tipo_taxa == 2) ? 'checked' : '') : ""; ?>>
+                    <label class="form-check-label" for="ATIVADO">
+                      PORCENTAGEM
+                    </label>
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div class="col-sm-4 col-lg-4">
-              <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-12 text-left control-label col-form-label">Data: <span style="color: red;" data-uw-styling-context="true">*</span></label>
-                <div class="col-sm-12 input-group has-validation">
-                  <input type="date" class="form-control" id="date" name="data_gasto" value="<?php echo isset($servico->descricao) ? $servico->data : ""; ?>" placeholder="" required>
+
+            
+              <div class="col-sm-6 col-lg-6">
+                <div class="form-group row ">
+                  <label for="validationCustom01" class="col-sm-12 text-left control-label col-form-label">Status: <span style="color: red;" data-uw-styling-context="true">*</span></label>
+                  <div class="col-sm-12  has-validation">
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" id="status" name="status" value="1" required <?php echo isset($f_pagamento->status) ? (($f_pagamento->status == 1) ? 'checked' : '') : ""; ?>>
+                      <label class="form-check-label">
+                        ATIVADO
+                      </label>
+                    </div>
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" id="status" name="status" value="2" required <?php echo isset($f_pagamento->status) ? (($f_pagamento->status == 2) ? 'checked' : '') : ""; ?>>
+                      <label class="form-check-label">
+                        DESATIVADO
+                      </label>
+                    </div>
+                    <input type="hidden" class="form-control" id="id" name="id" value="<?php echo isset($f_pagamento->id) ? $f_pagamento->id : ""; ?>">
+
+                  </div>
                 </div>
               </div>
-            </div>
+
           </div>
           <div class="card-footer text-center">
             <button type="submit" class="btn btn-primary mr-2">SALVAR</button>
